5e525cc685e77d2636e27c6d79c4f65c46206b37
oalmeida <octwalmeida@gmail.com>
2013-06-03 01:10:49 -0300

    getting info from facebook api

--- application/controllers/Portal.php
+++ application/controllers/Portal.php
@@ -9,23 +9,25 @@ class Portal extends MY_Controller{
 	
 	//gets the facebook login Url and then after login gets the user info
 	function facebookLogin_get(){
+		//load the user controller
+		$this->load->library('../controllers/User');
 		$userName = $this->post('userName');
 		$userPasswd = $this->post('password');
 		$this->load->library("FacebookFunctions");
-		$data['login'] = $this->facebookfunctions->getLoginUrl();
-		$info = $this->facebookfunctions->getInfo();
+		$ci = &get_instance();
+		$data['login'] = $ci->facebookfunctions->getLoginUrl();
+		$info = $ci->facebookfunctions->getInfo();
 		
 		//check if the info came back from facebook
 		if($info){
-			$this->load->library('../controllers/User');
-			$user = $this->user;
-			$user->setUserName($info['name']);
-			$user->setUserCity($info['location']['name']);
+			$this->user->setUserName($info['name']);
+			$this->user->setUserCity($info['location']['name']);
 			$data['info'] = $info;
 		}
 		$this->load->view("teste",$data);
 	}
 	
+	//function that will register new user without any other social network
 	function register_get(){
 		
 	}
@@ -44,8 +46,4 @@ class Portal extends MY_Controller{
 		$userPasswd = $this->post('password');
 	}
 	
-	function youtubeLogin(){
-		$userName = $this->post('userName');
-		$userPasswd = $this->post('password');
-	}
 }
